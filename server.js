f5384323dd1625b2ef348e014ef0a2a3785bee06
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-04 15:06:44 +0200

    Enable trust proxy in Express app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const app= express();
+app.set("trust proxy", true) ;
 app.get("/", (req,res)=> { console.log("IP visitante:", req.ip); res.send("hola mundo"); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

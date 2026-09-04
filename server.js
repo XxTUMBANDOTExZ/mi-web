a2575c85c148f61996a44e59efbda6bbdcd3b0a4
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-04 16:45:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
-app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent"); res.send(' <h1 style="font-size:60px;">TU IP FOR MY</h1>'); });
+app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent"); res.send('<h1 style="font-size:60px;">TU IP FOR MY</h1>'); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

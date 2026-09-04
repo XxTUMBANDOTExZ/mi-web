f79935b974e17e6fb4153140ce6df2344932b46a
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-04 15:46:43 +0200

    Fix typo in date formatting method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
-app.get("/", (req,res)=> { console.log("Visita:", new Date().tolSOString(), "IP:", req.ip); res.send("hola mundo"); });
+app.get("/", (req,res)=> { console.log("Visita:", new Date().tolSOstring(), "IP:", req.ip); res.send("hola mundo"); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

57893ebbd69851c11ac0ad3ddfe1ca155a1e7ab3
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-04 20:32:34 +0200

    Update background style in server response
    Changed background style from 'cover' to 'contain' for the static page.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
 app.use(express.static(__dirname));
-app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<body style="margin:0;min-height:100vh;background:url('/MI%20AMIGO.jpg') center/cover no-repeat;"><h1 style="font-size:60px;color:white;">TU IP FOR MY</h1></body>`); });
+app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<body style="margin:0;min-height:100vh;background:url('/MI%20AMIGO.jpg') center/contain no-repeat;"><h1 style="font-size:60px;color:white;">TU IP FOR MY</h1></body>`); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

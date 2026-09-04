20db9011013b7c18c533cf0d1cc018f09afc61a6
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-05 01:06:40 +0200

    Add GitHub link to homepage response
    Updated the HTML response to include a link to GitHub.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
 app.use(express.static(__dirname));
-app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<title>TU IP FOR MY</title><body style="margin:0;background:black;"><div style="position:fixed;left:20px;top:150px;color:white;">GitHub</div><h1 style="font-size:6vw;margin:0;color:white;text-align:center;font-family:Georgia,serif;">TU IP FOR MY</h1><p style="font-size:3vw;margin:0;text-align:center;color:white;text-align.center;font-family:monospace;">TU IP: ${req.ip}</p><img src="/MI%20AMIGO.jpg" style="max-width:90%;height:auto;display:block;margin:0 auto;"></body>`); });
+app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<title>TU IP FOR MY</title><body style="margin:0;background:black;"><div style="position:fixed;left:20px;top:150px;color:white;"><a href="https://github.com" target="_blank" style="color:white;"GitHub</a></div><h1 style="font-size:6vw;margin:0;color:white;text-align:center;font-family:Georgia,serif;">TU IP FOR MY</h1><p style="font-size:3vw;margin:0;text-align:center;color:white;text-align.center;font-family:monospace;">TU IP: ${req.ip}</p><img src="/MI%20AMIGO.jpg" style="max-width:90%;height:auto;display:block;margin:0 auto;"></body>`); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

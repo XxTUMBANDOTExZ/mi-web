49c260031b428261a3cf49f8ebaec798ba620c4a
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-04 16:06:52 +0200

    Update response message in root route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
-app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip); res.send("hola mundo"); });
+app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip); res.send(' <h1 style="font-size:60px;">TU IP FORM MY</h1>'); });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");

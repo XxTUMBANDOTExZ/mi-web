899cc3dea551e20da4332fb7f83dac41647feaa7
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-05 16:55:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express= require("express");
 const app= express();
 app.set("trust proxy", true) ;
 app.use(express.static(__dirname));
-app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<title>TU IP FOR MY</title><body style="background-color:black;"></div><h1 style="font-size:6vw;margin:0;color:white;text-align:center;font-family:Georgia,serif;">TU IP FOR MY</h1><p style="font-size:3vw;margin:0;text-align:center;color:white;text-align.center;font-family:monospace;">TU IP: ${req.ip}</p><img src="/MI%20AMIGO.jpg"
+app.get("/", (req,res)=> { console.log("Visita:", new Date().toISOString(), "IP:", req.ip, "Dispositivo:", req.get("user-agent")); res.send(`<title>TU IP FOR MY</title><body style="background-color:black;"></div><h1 style="font-size:6vw;margin:0;color:white;text-align:center;font-family:Georgia,serif;">TU IP FOR MY</h1><p style="color:#00ff88;font-family:monospace;text-align:center;">
+  > system online_
+</p><p style="font-size:3vw;margin:0;text-align:center;color:white;text-align.center;font-family:monospace;">TU IP: ${req.ip}</p><img src="/MI%20AMIGO.jpg"
 style="max-width:90%;height:auto;display:block;margin:20px auto;border:2px solid #00ff88;border-radius:12px;">
 
 <div style="text-align:center;color:white;margin-top:25px;">

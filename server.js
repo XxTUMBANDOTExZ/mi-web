0469ccb6a94b48484e708fd2297790b6ab9b1298
XxTUMBANDOTExZ <xxlliillii2012lliilliixx@gmail.com>
2026-09-07 22:36:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ text-align:center;
     ">
 
       <img
-      src="href="https://youtu.be/atVsVq8W4Js"/maxresdefault.jpg"
+      src="https://img.youtube.com/vi/atVsVq8W4Js/maxresdefault.jpg"
       style="width:100%;border-radius:8px;"
       alt="Miniatura vídeo IP">
 
